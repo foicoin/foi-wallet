f3a16203b942da1386b5ff70a88243242c58ccd2
Ubuntu <testuser@foimist.yvq0u1n0s0uurgtb03qnsvmrgb.xx.internal.cloudapp.net>
2018-04-02 07:25:49 +0000

    merge readme edit

--- main.js
+++ main.js
@@ -128,7 +128,7 @@ app.on('ready', async () => {
     dialog.showErrorBox(
       'Insecure RPC connection',
       `
-WARNING: You are connecting to an Ethereum node via: ${Settings.rpcHttpPath}
+WARNING: You are connecting to an Foicoin node via: ${Settings.rpcHttpPath}
 
 This is less secure than using local IPC - your passwords will be sent over the wire in plaintext.
 
--- modules/ipc/ipcProviderBackend.js
+++ modules/ipc/ipcProviderBackend.js
@@ -161,7 +161,7 @@ class IpcProviderBackend {
                       ethereumNode.removeListener('state', onStateChange);
 
                       log.debug(
-                        `Ethereum node connected, resume connecting socket ${ownerId}`
+                        `Foicoin node connected, resume connecting socket ${ownerId}`
                       );
 
                       resolve();
@@ -215,7 +215,7 @@ class IpcProviderBackend {
   }
 
   /**
-   * Handler for when Ethereum node state changes.
+   * Handler for when Foicoin node state changes.
    *
    * Auto-reconnect sockets when ethereum node state changes
    *
@@ -225,7 +225,7 @@ class IpcProviderBackend {
     switch (state) { // eslint-disable-line default-case
       // stop syncing when node about to be stopped
       case ethereumNode.STATES.STOPPING:
-        log.info('Ethereum node stopping, disconnecting sockets');
+        log.info('Foicoin node stopping, disconnecting sockets');
 
         Q.all(
           _.map(this._connections, item => {
