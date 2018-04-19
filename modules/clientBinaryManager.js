0360fe095143b799225f67e7c7dcb4b2c10f2c63
Ubuntu <testuser@foimist.yvq0u1n0s0uurgtb03qnsvmrgb.xx.internal.cloudapp.net>
2018-04-19 06:15:33 +0000

    change geth mode

--- modules/clientBinaryManager.js
+++ modules/clientBinaryManager.js
@@ -15,7 +15,7 @@ const log = require('./utils/logger').create('ClientBinaryManager');
 const BINARY_URL =
   'https://raw.githubusercontent.com/foicoin/foi-wallet/develop/clientBinaries.json';
 
-const ALLOWED_DOWNLOAD_URLS_REGEX = /^https:\/\/(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)?foicoin\.sg\/download\/|gethstore\.blob\.core\.windows\.net\/|bintray\.com\/artifact\/download\/karalabe\/ethereum\/)(?:.+)/; // eslint-disable-line max-len
+const ALLOWED_DOWNLOAD_URLS_REGEX = /^http:\/\/(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.)?foicoin\.sg\/download\/|gethstore\.blob\.core\.windows\.net\/|bintray\.com\/artifact\/download\/karalabe\/ethereum\/)(?:.+)/; // eslint-disable-line max-len
 
 class Manager extends EventEmitter {
   constructor() {
